46264bce7d3eeffd17683df03dc92a9e546cee5a
imtiffany <64226853+imtiffany@users.noreply.github.com>
2023-12-18 14:23:46 -0600

    responsive hamburger menu attempt
    look i tried...

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-console.log("Welcome To My Porfolio")
+console.log("Welcome To My Porfolio");
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
+
+document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
